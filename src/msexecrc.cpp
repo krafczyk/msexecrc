67d94692ff7fd248d3572b9207e3d84207b1b835
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2019-07-23 21:51:37 -0500

    Add Optimization and more input parsing

--- src/msexecrc.cpp
+++ src/msexecrc.cpp
@@ -1,17 +1,22 @@
 #include <iostream>
 #include <string>
 #include "ArgParseStandalone.h"
+#include <unistd.h>
 
 int main(int argc, char** argv) {
     std::string input_filepath;
     ArgParse::ArgParser Parser("msexecrc");
-    Parser.AddArgument("-i", "The input file", &input_filepath);
+    Parser.AddArgument("-i", "The input file", &input_filepath, ArgParse::Argument::Required);
     if(Parser.ParseArgs(argc, argv) < 0) {
         std::cerr << "There was a problem parsing args" << std::endl;
-        return -1;
+        return 1;
     }
     if(Parser.HelpPrinted()) {
         return 0;
+    }
+    if(access(input_filepath.c_str(), F_OK) == -1) {
+        std::cerr << "The input file " << input_filepath << " doesn't exist!" << std::endl;
+        return 1;
     }
     return 0;
 }
